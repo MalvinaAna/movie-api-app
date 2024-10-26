a08feaa376975df91e4eea1e9ad70318bf8523a1
MalvinaAna <47119139+MalvinaAna@users.noreply.github.com>
2024-10-26 22:34:35 +0300

    documentation

--- auth.js
+++ auth.js
@@ -5,7 +5,12 @@ const jwt = require('jsonwebtoken'),
 
   require('./passport'); // Simply require the passport file
 
-
+/**
+ * Generates a JSON Web Token (JWT) for a given user.
+ * @param {Object} user - The user object containing the user's information.
+ * @param {string} user.Username - The username of the user to encode in the JWT.
+ * @returns {string} - The signed JWT as a string.
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
@@ -15,7 +20,11 @@ let generateJWTToken = (user) => {
 }
 
 
-/* POST login. */
+/**
+ * POST login route. Authenticates the user and generates a JWT upon successful login.
+ * @param {Object} router - The Express router object.
+ * @returns {void}
+ */
 module.exports = (router) => {
   router.post('/login', (req, res) => {
     passport.authenticate('local', {session: false}, (error, user, info) => {
